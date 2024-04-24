03dbc4b68a4f03eaacd60ecd6494c371bee4bb16
Dmitrii Ostapenko <mikeikerson@yandex.ru>
2024-04-24 22:20:58 +0300

    code refactoring

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -5,17 +7,15 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.clock import Clock
 
-import datetime
-
 
 class SingleStopwatch(BoxLayout):
 
     def __init__(self, **kwargs):
-        super(SingleStopwatch, self).__init__(**kwargs)
-        self.update_interval = kwargs.get('update_interval', 1)
+        self.update_interval = kwargs.pop('update_interval', 1)
+        super().__init__(**kwargs)
+        print(self.update_interval)
         self.time = datetime.timedelta(seconds=0)
-        self.timer_trigger = Clock.schedule_interval(self.update_time, self.update_interval)
-        self.timer_trigger.cancel()
+        self.timer = None
 
         layout = BoxLayout(orientation='vertical')
         self.time_label = Label(text='00:00:00')
@@ -30,9 +30,9 @@ class SingleStopwatch(BoxLayout):
         btn_reset = Button(text='Reset')
         buttons_layout.add_widget(btn_reset)
 
-        btn_start.bind(on_release=self.start)
-        btn_stop.bind(on_release=self.stop)
-        btn_reset.bind(on_release=self.reset)
+        btn_start.bind(on_release=self.on_start)
+        btn_stop.bind(on_release=self.on_stop)
+        btn_reset.bind(on_release=self.on_reset)
 
         layout.add_widget(buttons_layout)
 
@@ -41,27 +41,38 @@ class SingleStopwatch(BoxLayout):
         hours = self.time.seconds // 3600
         minutes = (self.time.seconds % 3600) // 60
         seconds = self.time.seconds % 60
-        self.time_label.text = f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+        self.time_label.text = f"{hours:0>2}:{minutes:0>2}:{seconds:0>2}"
 
-    def start(self, *args):
-        if not self.timer_trigger.is_triggered:
-            self.timer_trigger = Clock.schedule_interval(self.update_time, self.update_interval)
+    def start(self):
+        if self.timer is None:
+            self.timer = Clock.schedule_interval(self.update_time, self.update_interval)
 
-    def stop(self, *args):
-        self.timer_trigger.cancel()
+    def stop(self):
+        if self.timer is not None:
+            self.timer.cancel()
+            self.timer = None
 
-    def reset(self, *args):
-        self.timer_trigger.cancel()
+    def reset(self):
+        self.stop()
         self.time = datetime.timedelta(seconds=0)
         self.time_label.text = "00:00:00"
+
+    def on_start(self, button):
+        self.start()
+
+    def on_stop(self, button):
+        self.stop()
+
+    def on_reset(self, button):
+        self.reset()
 
 
 class MainScreen(GridLayout):
 
     def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.cols = 1
-        stopwatch = SingleStopwatch()
+        stopwatch = SingleStopwatch(update_interval=0.1)
         self.add_widget(stopwatch)
 
 
